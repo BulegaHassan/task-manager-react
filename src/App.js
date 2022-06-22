1687087333a4d62944741bd6eeac3aaedd292d59
Hassan Bulega <hassanbulega@gmail.com>
2022-06-22 12:49:54 +0300

    chore: add props for alert component

--- src/App.js
+++ src/App.js
@@ -10,43 +10,49 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-const handleSubmit = (e)=> {
-  e.preventDefault()
-  if(!name){
-    //display alert
-  }
-  else if(name && isEditing){
-    // deal with edit
-  }
-  else {
-    // show alert
-    const newItem = {id: uuidv4(),title:name}
-    setList([...list,newItem])
-    setName('')
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+     showAlert(true,'danger','please enter value')
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: uuidv4(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+const showAlert = (show=false,type="",msg="")=> {
+  setAlert({show,type,msg})
 }
-
   return (
-    <section className="section-center">
-      <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert/>}
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>task manager</h3>
-        <div className="form-control">
-          <input type="text" className="grocery" placeholder="e.g wash clothes" value={name} onChange={(e)=> setName(e.target.value)} />
-          <button className="submit-btn">
-            {isEditing?'edit':'submit'}
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g wash clothes'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className='submit-btn'>
+            {isEditing ? "edit" : "submit"}
           </button>
         </div>
       </form>
       {list.length > 0 && (
-
-      <div className="grocery-contain">
-      <List items={list}/>
-      <button className="clear-btn">clear items</button>
-      </div>
+        <div className='grocery-contain'>
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
       )}
     </section>
-  )
+  );
 }
 
 export default App;
