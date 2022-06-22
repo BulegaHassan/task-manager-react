c8301b32f4fcfe8addcc9431d302248230b54d0d
Hassan Bulega <hassanbulega@gmail.com>
2022-06-22 14:17:06 +0300

    feat: add eventHandler for delete icon

--- src/List.js
+++ src/List.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
-  return <div className='grocery-list'>
-    {items.map((item) => {
-      const{id,title}=item
-     
-      return (
-        <article className='grocery-item'>
-          <p className='title'>{title}</p>
-          <div className='btn-container'>
-            <button type='button' className='edit-btn'>
-              <FaEdit />
-            </button>
-            <button type='button' className='delete-btn'>
-              <FaTrash />
-            </button>
-          </div>
-          
-        </article>
-      );
-    })}
-    </div>;
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items, removeItem}) => {
+  return (
+    <div className='grocery-list'>
+      {items.map((item) => {
+        const { id, title } = item;
 
-export default List
+        return (
+          <article className='grocery-item'>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button type='button' className='edit-btn'>
+                <FaEdit />
+              </button>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
